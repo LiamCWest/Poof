8f9a77d152c946201bcfddac9d24465ee89ad96f
Heybooyah <80083361+Heybooyah@users.noreply.github.com>
2023-12-18 13:52:30 -0500

    added debug

---
+++ utils/drawDebug.py
@@ -0,0 +1,25 @@
+import pygame as pg
+
+def createDebugTexture(filename, size = 8, c1 = (0,0,0), c2 = (255,0,255)):
+    
+    try:
+        open(filename)
+        
+    except:
+        surface = pg.Surface((size,size))
+
+        half = size / 2
+        pg.draw.rect(surface,c1,(0,0,size,size))
+        
+        pg.draw.rect(surface,c2,(half,0,half,half))
+        pg.draw.rect(surface,c2,(0,half,half,half))
+        
+        pg.image.save(surface, filename)
+        
+        try:
+            open(filename)
+            
+        except:
+            print("Error creating debug texture")
+            
+    return filename

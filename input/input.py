02af49502999b01c9facf6c1692782835fc6a4ea
archiso <awesomelax7@gmail.com>
2023-12-19 23:03:56 -0500

    pynput input instead of keyboard

--- input/input.py
+++ input/input.py
@@ -1,14 +1,12 @@
-import keyboard
+from pynput import keyboard
 import pygame
 import logic.song.songPlayer as songPlayer
 
 class Event:
-    def __init__(self, key, eventHandler):
+    def __init__(self, key):
         self.key = key
-        self.handler = eventHandler #so the key can be unbinded later
         
         self.pressed = False
-        
         self.songTimeLastPressed = None
         self.songTimeLastReleased = None
         self.realTimeLastPressed = None
@@ -35,52 +33,51 @@ class Event:
     def setJustReleased(self, val):
         self.__justReleased = val
     
-    justReleased = property(fget=getJustReleased, fset = setJustReleased)
+    justReleased = property(fget=getJustReleased, fset=setJustReleased)
 
 keybinds = {
-    "left" : Event(None, None),
-    "up" : Event(None, None),
-    "right" : Event(None, None),
-    "down" : Event(None, None),
-    "dash" : Event(None, None)
+    "left": Event("'a'"),
+    "up": Event("'w'"),
+    "right": Event("'d'"),
+    "down": Event("'s'"),
+    "dash": Event("'shift'"),
 }
 
-def bind(action, key):
+def on_press(key):
+    print(key)
     global keybinds
-    def callback(event, action):
-        global keybinds
-        pressed = event.event_type == keyboard.KEY_DOWN
-        
-        if pressed == keybinds[action].pressed:
-            return
-        
-        songTime = songPlayer.getPos() if songPlayer.getIsPlaying() else None
-        
-        ticks = pygame.time.get_ticks()
-        realTime = ticks / 1000 if ticks != 0 else None
-            
-        if pressed:
-            keybinds[action].songTimeLastPressed = songTime
-            keybinds[action].realTimeLastPressed = realTime
-            keybinds[action].pressed = True
-            keybinds[action].justPressed = True
-            keybinds[action].justReleased = False
-        else:
-            keybinds[action].songTimeLastReleased = songTime
-            keybinds[action].realTimeLastReleased = realTime
-            keybinds[action].pressed = False
-            keybinds[action].justReleased = True
-            keybinds[action].justPressed = False
-            
-        
-    if keybinds[action].handler is not None:
-        keyboard.unhook_key(keybinds[action].handler)
-    newHandler = keyboard.hook_key(key, lambda event, act=action, : callback(event, act))
-    keybinds[action] = Event(key, newHandler)
     
+    songTime = songPlayer.getPos() if songPlayer.getIsPlaying() else None
+        
+    ticks = pygame.time.get_ticks()
+    realTime = ticks / 1000 if ticks != 0 else None
+    
+    for action, event in keybinds.items():
+        if str(key) == event.key and not event.pressed:
+            print("pressed")
+            event.songTimeLastPressed = songTime
+            event.realTimeLastPressed = realTime
+            event.pressed = True
+            event.justPressed = True
+            event.justReleased = False
+
+def on_release(key):
+    print(key)
+    global keybinds
+    
+    songTime = songPlayer.getPos() if songPlayer.getIsPlaying() else None
+        
+    ticks = pygame.time.get_ticks()
+    realTime = ticks / 1000 if ticks != 0 else None
+    
+    for action, event in keybinds.items():
+        if str(key) == event.key:
+            event.songTimeLastPressed = songTime
+            event.realTimeLastPressed = realTime
+            event.pressed = False
+            event.justReleased = True
+            event.justPressed = False
+
 def init():
-    bind("left", "a")
-    bind("right", "d")
-    bind("up", "w")
-    bind("down", "s")
-    bind("dash", "shift")
+    listener = keyboard.Listener(on_press=on_press, on_release=on_release)
+    listener.start()
--- test.py
+++
@@ -1,9 +0,0 @@
-import keyboard
-
-def f(ev):
-    print("pressed")
-    print(ev.event_type == keyboard.KEY_UP)
-    
-keyboard.hook_key("w", f)
-while True:
-    continue

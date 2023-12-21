6b7f4b6094dbba4fe58ce27b296e9f64df8840b1
Heybooyah <80083361+Heybooyah@users.noreply.github.com>
2023-12-21 12:56:00 -0500

    z layering (probably works i didnt really test)

--- ui/text.py
+++ ui/text.py
@@ -1,13 +1,14 @@
 import graphics.gui as gui
 
 class Text:
-    def __init__(self, text, x, y, color, size, font = "Arial"):
+    def __init__(self, text, x, y, color, size, font = "Arial", z = 0):
         self.text = text
         self.x = x
         self.y = y
         self.color = color
         self.font = font
         self.size = size
+        self.z = z
 
     def draw(self):
         gui.drawText(self.text, self.x, self.y, self.size, self.color, self.font)

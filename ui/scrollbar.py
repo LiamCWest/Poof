f60082a88ffb5f793d300f3c68d792f5f4e8382e
archiso <awesomelax7@gmail.com>
2023-12-24 15:18:53 -0500

    small update to scrollbar

--- ui/scrollbar.py
+++ ui/scrollbar.py
@@ -2,12 +2,11 @@ import pygame
 from ui.button import Button
 
 class Scrollbar:
-    def __init__(self, x, y, width, height, totalHeight, orientation, bg = (0, 0, 0), fg = (255, 255, 255)):
+    def __init__(self, x, y, width, height, orientation, bg = (0, 0, 0), fg = (255, 255, 255)):
         self.x = x
         self.y = y
         self.width = width
         self.height = height
-        self.totalHeight = totalHeight
         self.orientation = orientation
         self.bg = bg
         self.fg = fg
@@ -16,7 +15,7 @@ class Scrollbar:
         self.value = 0
         
     def draw(self, screen): #TODO: orientation fix
-        pygame.draw.rect(screen, self.bg, (self.x, self.y, self.width, self.totalHeight))
+        pygame.draw.rect(screen, self.bg, (self.x, self.y, self.width, self.height))
     
     def dragStart(self):
         while self.bar.held:

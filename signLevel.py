a2a9010f000f3ddc8f9aa9b7e01ef2e92179552c
archiso <awesomelax7@gmail.com>
2024-01-07 20:30:45 -0500

    signLevel util for resigning levels when manually modifying them

---
+++ signLevel.py
@@ -0,0 +1,24 @@
+import json
+import hashlib
+
+def signData(data):
+    signature = hashlib.sha256(json.dumps(data).encode()).hexdigest()
+    return signature
+
+def checkSignature(data, signature):
+    return signature == signData(data)
+
+level = input("Level file: ")
+
+with open (level, 'r') as file:
+    saved_data = json.load(file)
+    loaded_data = saved_data['data']
+    saved_signature = saved_data['signature']
+    if not checkSignature(loaded_data, saved_signature):
+        print("Level file corrupted, resigning...")
+        newSig = signData(loaded_data)
+        with open(level, 'w') as file:
+            json.dump({"data": loaded_data, "signature": newSig}, file)
+    else:
+        print("Level file is valid")
+        

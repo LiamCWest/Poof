b7c7e76ada267472130f011372cabc96845e5bee
Heybooyah <80083361+Heybooyah@users.noreply.github.com>
2024-01-15 12:35:08 -0500

    gui comments and particle comments

--- graphics/gui.py
+++ graphics/gui.py
@@ -1,85 +1,85 @@
 # external imports
-import pygame
-import ctypes
-import platform
+import pygame #import the pygame module
+import ctypes #import the ctypes module
+import platform #import the platform module
 
 # internal imports
-import logic.level.levelEditor as levelEditor
-import input.input as input
-from logic.game import game
-from utils.stack import Stack
-from fonts.fonts import getFont
-from ui.menus import mainMenu, settingsMenu, levelMenu
+import logic.level.levelEditor as levelEditor #import the level editor
+import input.input as input #import the input system
+from logic.game import game #import the game loop system
+from utils.stack import Stack #import the stack class
+from fonts.fonts import getFont #import the getFont class
+from ui.menus import mainMenu, settingsMenu, levelMenu #import the various menu classes
 
-screen = None
-screens = None
-activeScreen = None
-activeScreenName = None
-screenStack = Stack()
-def init():
+screen = None #default the screen to none
+screens = None #default the screen list to none
+activeScreen = None #default the active screen to none
+activeScreenName = None #default the name of the active screen to none
+screenStack = Stack() #create an empty stack for the screens
+def init(): #initialize the GUI system
     if platform.system() == "Windows": ctypes.windll.user32.SetProcessDPIAware() #makes window not scale with display scaling
     
-    global screen, screens, activeScreen, activeScreenName
-    screen = pygame.display.set_mode((1280, 720), pygame.SCALED)
-    screen.fill((255, 255, 255))
+    global screen, screens, activeScreen, activeScreenName #move these variables to the global scope
+    screen = pygame.display.set_mode((1280, 720), pygame.SCALED) #create the diplay screen
+    screen.fill((255, 255, 255)) #fill the screen with a white background
     
-    screens = {"main": mainMenu, "game": game, "settings": settingsMenu, "levelEditor": levelEditor, "levelMenu": levelMenu}
-    setScreen("main")
+    screens = {"main": mainMenu, "game": game, "settings": settingsMenu, "levelEditor": levelEditor, "levelMenu": levelMenu} #create screens for the menus
+    setScreen("main") #set the screen to the main menu
 
-def setScreen(name, back = False):
-    global activeScreen, activeScreenName, screenStack
+def setScreen(name, back = False): #this sets the screen to the inputted screen
+    global activeScreen, activeScreenName, screenStack #move these variables to the global scope
     
-    if activeScreen:
-        activeScreen.hide()
+    if activeScreen: #if there is an active screen
+        activeScreen.hide() #hide the active screen
     
-    if not back: screenStack.push(activeScreenName)
-    activeScreenName = name
-    activeScreen = screens[activeScreenName]
+    if not back: screenStack.push(activeScreenName) #if not moving back, add the screen to the stack
+    activeScreenName = name #set the active screen name to the name of the screen
+    activeScreen = screens[activeScreenName] #set the active screen
     
-    activeScreen.show()
+    activeScreen.show() #show the active screen
 
-def back():
-    global screenStack
-    if screenStack.peek: setScreen(screenStack.pop(), back = True)
+def back(): #this handles moving backwards in the screen stack
+    global screenStack #move screenStack to the global scope
+    if screenStack.peek: setScreen(screenStack.pop(), back = True) #if the screen stack isn't empty, pop the current screen and set the new screen
 
-def checkInput():
-    if input.specialKeyBindings["escape"].justPressed:
-        if activeScreenName not in ["game", "main"]:
-            if activeScreen.popupOpen:
-                activeScreen.popupClose()
-            else:
-                back()
-        elif activeScreenName == "game":
-            if activeScreen.popupOpen:
-                activeScreen.resume()
-            else:
-                activeScreen.pause()
+def checkInput(): #handle various inputs
+    if input.specialKeyBindings["escape"].justPressed: #if escape key is pressed
+        if activeScreenName not in ["game", "main"]: #and the active screen isn't "game" or "main" (the player isn't mid-game or on the main menu)
+            if activeScreen.popupOpen: #if the active screen has a popup open
+                activeScreen.popupClose() #close the popup
+            else: #if the active screen doesn't have a popup open
+                back() #move the active screen back in the screen stack
+        elif activeScreenName == "game": #if the active screen is the game window
+            if activeScreen.popupOpen: #if the active screen has a popup open
+                activeScreen.resume() #close the popup and resume gameplay
+            else: #if there is no current popup
+                activeScreen.pause() #pause the game and create the pause popup
 
-def drawText(text, x, y, size, color, font, cutOff = None):
-    global screen
-    text_surface = getFont(font, size).render(text, True, color)
-    text_rect = text_surface.get_rect(center=(x, y))
+def drawText(text, x, y, size, color, font, cutOff = None): #this function draws text to the screen
+    global screen #get the screen from the global scope
+    text_surface = getFont(font, size).render(text, True, color) #create a new surface for the text font
+    text_rect = text_surface.get_rect(center=(x, y)) #create the rectangle that contains the text
 
-    if cutOff:
-        cutOff_rect = pygame.Rect(cutOff).clip(screen.get_rect())
-        if text_rect.colliderect(cutOff_rect):
-            cutOff_surface = screen.subsurface(cutOff_rect)
-            cutOff_surface.blit(text_surface, text_rect.move(-cutOff_rect.topleft[0], -cutOff_rect.topleft[1]))
-    else:
-        screen.blit(text_surface, text_rect)
+    if cutOff: #if the text will be cut off
+        cutOff_rect = pygame.Rect(cutOff).clip(screen.get_rect()) #clip the part of the screen that will be cut off
+        if text_rect.colliderect(cutOff_rect): #if the text collides with the cut off part
+            cutOff_surface = screen.subsurface(cutOff_rect) #create a new surface for the new cutOff surface
+            cutOff_surface.blit(text_surface, text_rect.move(-cutOff_rect.topleft[0], -cutOff_rect.topleft[1])) #blit the cut off surface
+    else: #if the text is not cut off
+        screen.blit(text_surface, text_rect) #blit the text onto the screen
     
-def clear():
-    global screen
-    screen.fill((55, 55, 55))
+def clear(): #clears the screen
+    global screen #retrieve the screen from globals
+    screen.fill((55, 55, 55)) #fill the screen with dark grey
     
-def update():
-    global activeScreen
-    checkInput()
-    if activeScreen: 
-        activeScreen.update()
-    draw()
+def update(): #updates the screen
+    global activeScreen #retrieve the active screen from globals
+    checkInput() #check and handle input events
+    if activeScreen: #if there is an active screen
+        activeScreen.update() #update the active screen
+    draw() #draw the entire screen
     
-def draw():
-    global activeScreen
-    clear()
-    if activeScreen: activeScreen.draw()
+def draw(): #draws the screen
+    global activeScreen #retrieve the active screen from globals
+    clear() #clear the game window
+    if activeScreen: activeScreen.draw() #draw the screen if it exists
--- graphics/particleSystem/emitter.py
+++ graphics/particleSystem/emitter.py
@@ -1,44 +1,44 @@
 # external imports
-import random
+import random #imports the random module
 
 # internal imports
-from graphics.particleSystem.particle import Particle
-from utils.vector2 import Vector2
-import input.input as input
+from graphics.particleSystem.particle import Particle #import the particle class
+from utils.vector2 import Vector2 #import the vector2 class
+import input.input as input #import the input system
 
-class Emitter:
-    def __init__(self, pos, velocity, emitRate, lifeTime, size=5, limit = 200):
-        self.pos = pos
-        self.velocity = velocity
-        self.emitRate = emitRate
-        self.lifeTime = lifeTime
-        self.size = size
-        self.limit = limit
-        self.particles = []
-        self.lastSpawnTime = 0
+class Emitter: #create the emitter class, which spawns particles
+    def __init__(self, pos, velocity, emitRate, lifeTime, size=5, limit = 200): #initialize the emitter
+        self.pos = pos #initialize the position
+        self.velocity = velocity #initialize the velocity
+        self.emitRate = emitRate #initialize the emit rate
+        self.lifeTime = lifeTime #initialize the life time
+        self.size = size #initialize the size of the particle
+        self.limit = limit #initialize the particle object limit
+        self.particles = [] #initialize the particle list, which contains the emitter's particles
+        self.lastSpawnTime = 0 #initialize the last spawn time of any particle
 
-    def emit(self):
-        if len(self.particles) < self.limit:
-            self.particles.append(self.makeParticle())
+    def emit(self): #emit a particle
+        if len(self.particles) < self.limit: #check if we have enough particles
+            self.particles.append(self.makeParticle()) #create a new particle
 
-    def makeParticle(self):
-        return Particle(self.pos, Vector2(random.uniform(-1, 1), random.uniform(-1, 1)) * self.velocity, self.lifeTime, self.size)
+    def makeParticle(self): #create a particle
+        return Particle(self.pos, Vector2(random.uniform(-1, 1), random.uniform(-1, 1)) * self.velocity, self.lifeTime, self.size) #return a new particle
 
-    def update(self):
-        t = input.getRealTime() - self.lastSpawnTime
-        if t <= (1/self.emitRate) * 5:
-            while t > (1/self.emitRate):
-                self.emit()
-                t -= 1/self.emitRate
-        self.lastSpawnTime = input.getRealTime()
-        for particle in self.particles:
-            particle.update()
-            if particle.lifeTime <= 0:
-                self.particles.remove(particle)
+    def update(self): #update the emitter (create particles if need be)
+        t = input.getRealTime() - self.lastSpawnTime #get the time since the last spawn
+        if t <= (1/self.emitRate) * 5: #check if it's time to spawn a particle
+            while t > (1/self.emitRate): #while there are more particles to spawn
+                self.emit() #emit a particle
+                t -= 1/self.emitRate #increment t
+        self.lastSpawnTime = input.getRealTime() #set the new last spawn time
+        for particle in self.particles: #for each particle in the list
+            particle.update() #update the particle
+            if particle.lifeTime <= 0: #if the particle should be removed
+                self.particles.remove(particle) #remove the particle
             
-    def draw(self, win, pos = Vector2(0,0)):
-        for particle in self.particles:
-            particle.draw(win, pos)
+    def draw(self, win, pos = Vector2(0,0)): #draw the particles
+        for particle in self.particles: #for each particle in the list
+            particle.draw(win, pos) #draw the particle
 
-    def reset(self):
-        self.particles = []
+    def reset(self): #this empties the particle list
+        self.particles = [] #empty the particle list
